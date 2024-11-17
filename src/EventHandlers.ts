779115b0883872a0dd98229792eb604edc23a699
denhampreen <denhampreen@gmail.com>
2024-11-17 16:20:23 +0700

    fix: sanitize all invalid null byte pg strings

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -10,11 +10,11 @@ import { getTokenDetails } from "./tokenDetails";
 UniswapV3Factory.PoolCreated.handler(async ({ event, context }) => {
   const entity: Pool = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    token0_id: event.params.token0,
-    token1_id: event.params.token1,
+    token0_id: event.params.token0.replace(/\0/g, ''),
+    token1_id: event.params.token1.replace(/\0/g, ''),
     fee: event.params.fee,
     tickSpacing: event.params.tickSpacing,
-    pool: event.params.pool,
+    pool: event.params.pool.replace(/\0/g, ''),
   };
   context.Pool.set(entity)
 
@@ -22,11 +22,11 @@ UniswapV3Factory.PoolCreated.handler(async ({ event, context }) => {
   try {
     const {name: name0, symbol: symbol0, decimals: decimals0} = await getTokenDetails(event.params.token0, event.chainId, event.params.pool);
     context.Token.set({
-      id: event.params.token0,
-      name: name0,
-      symbol: symbol0,
+      id: event.params.token0.replace(/\0/g, ''),
+      name: name0.replace(/\0/g, ''),
+      symbol: symbol0.replace(/\0/g, ''),
       decimals: decimals0
-    })
+    })    
   } catch (error) {
     console.log('failed token0 with address', event.params.token0)
     return
@@ -35,9 +35,9 @@ UniswapV3Factory.PoolCreated.handler(async ({ event, context }) => {
   try {
     const {name: name1, symbol: symbol1, decimals: decimals1} = await getTokenDetails(event.params.token1, event.chainId, event.params.pool);
     context.Token.set({
-      id: event.params.token1,
-      name: name1,
-      symbol: symbol1,
+      id: event.params.token1.replace(/\0/g, ''),
+      name: name1.replace(/\0/g, ''),
+      symbol: symbol1.replace(/\0/g, ''),
       decimals: decimals1,
     })
   } catch (error) {
